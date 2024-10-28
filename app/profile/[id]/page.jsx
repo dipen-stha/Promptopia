9944c1eb0ba58b3889cdd7bbebae69477bcea2ff
Dipen Shrestha <dipenstha00@gmail.com>
2024-10-28 22:34:00 +0545

    fix: remove unwanted codes

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -21,28 +21,7 @@ const UserProfile = ({ params }) => {
     
         if (params?.id) fetchPosts();
       }, []);
-    const handleEdit = (post) => {
-      router.push(`/update-prompt?id=${post._id}`)
-    }
 
-    const handleDelete = async (post) =>{
-      const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
-
-      if(hasConfirmed){
-        try {
-          await fetch(`/api/prompt/${post._id.toString()}`, {
-            method: 'DELETE'
-          });
-
-          const filteredPosts = posts.filter((p) => p._id !== post._id)
-
-          setPosts(filteredPosts);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
-    }
   return (
     <Profile
     name={userName}
