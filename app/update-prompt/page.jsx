070a509da1a8de428e99026c1301a86bee0af28b
Dipen Shrestha <dipenstha00@gmail.com>
2024-10-28 22:24:29 +0545

    fix: minor typo changes

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,19 +1,19 @@
-'use client';
+"use client";
 
-import  { useState, useEffect } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import  { useState, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 
-import Form from '@components/Form';
+import Form from "@components/Form";
 
 const EditPrompt = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
-    const promptId = searchParams.get('id');
+    const promptId = searchParams.get("id");
 
     const [submitting, setSubmitting] = useState(false)
     const [post, setPost] = useState({
-        prompt: '',
-        tag: '',
+        prompt: "",
+        tag: "",
     });
 
     useEffect(() => {
@@ -32,18 +32,18 @@ const EditPrompt = () => {
         e.preventDefault();
         setSubmitting(true);
 
-        if(!promptId) return alert('Prompt Id not found')
+        if(!promptId) return alert("Prompt Id not found")
 
         try{
             const response = await fetch(`/api/prompt/${promptId}`, {
-                method: 'PATCH',
+                method: "PATCH",
                 body: JSON.stringify({
                     prompt: post.prompt,
                     tag: post.tag
                 })
             })
             if(response.ok){
-                router.push('/');
+                router.push("/");
             }
         }
         catch(error){
